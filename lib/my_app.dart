b46475dff140d7d0175a00e6e3f83d0721101d34
Ahmed gamal shalaby <ahmed.gamal@gmail.com>
2024-07-25 11:44:18 +0300

    add my_app file

---
+++ lib/my_app.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:after_layout/after_layout.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+ import 'common/constants/app_theme.dart';
+import 'common/constants/providers.dart';
+import 'common/helper/navigation/router.dart' as router;
+import 'features/onboarding/presentation/view/screens/splash_screen.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>
+    with AfterLayoutMixin, WidgetsBindingObserver {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: providersList,
+        child: ScreenUtilInit(
+          designSize: const Size(375, 812),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) => MaterialApp(
+            navigatorKey: navigatorKey,
+            debugShowCheckedModeBanner: false,
+            title: "TrueDar",
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            theme: buildAppTheme(context),
+            home: const SplashScreen(),
+            onGenerateRoute: router.generateRoute,
+          ),
+        ));
+  }
+
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) {
+    WidgetsBinding.instance.addObserver(this);
+  }
+}
