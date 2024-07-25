a5dd06a86bad8b1a73631c8ea5932fea77bf313b
Ahmed gamal shalaby <ahmed.gamal@gmail.com>
2024-07-25 11:27:24 +0300

    Init some colors in app colors class

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //grey shades
+  static const Color grey = Color(0xFF8e8e8e);
+  static const Color grey1 = Color(0xFFEFEFEF);
+  static const Color greyBG = Color(0xFFDFDFDF);
+  static const Color blackBG = Color(0xFF171C26);
+  static const Color themeColorBody = Color(0xff6E6B7B);
+  static const Color textColorChat = Color(0xFFEEEEEE);
+  static const Color onLight800 = Color(0xFF252B41);
+  static const Color transparent = Colors.transparent;
+  static const Color themeSwitchActive = Color(0xFF24272C);
+  static const Color primary = Color(0XFFFFAA45);
+  static const Color lighterPrimary = Color(0XFFFFCC90);
+  static const Color darkerPrimary = Color(0XFFBD6C0C);
+  static const Color secondary = Color(0XFF48525B);
+  static const Color backGround = Color(0xFF252B5C);
+  static const Color greyMedium = Color(0xFF53587A);
+  static const Color greyBarelyMedium = Color(0xFFA1A5C1);
+  static const Color typeOnProperty = Color(0xFF234F68);
+  static const Color greySoft = Color(0xFFF5F4F8);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color greenCyan = Color(0xFF337E89);
+}
